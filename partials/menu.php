9074cd328c99de2852fc4faefe74e4ef4bcf1474
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-27 16:56:38 -0300

    Ajuste do menu para funcionar com o navwalker do Bootstrap 4

--- partials/menu.php
+++ partials/menu.php
@@ -1,15 +1,25 @@
 <a href="#inicio-menu" id="inicio-menu" class="sr-only">In&iacute;cio do menu</a>
 
-<button class="btn btn-menu-toggle btn-lg d-block mx-auto d-lg-none"><span class="sr-only">Esconder/Mostrar&nbsp;</span>Menu</button>
-<?php
-    wp_nav_menu( array(
-        'theme_location'    => 'main',
-        'depth'             => 2,
-        'container'         => 'nav',
-        'container_class'   => 'menu-navbar collapse show',
-        'container_id'      => false,
-        'menu_class'        => 'menu-principal nav flex-column',
-    ));
-?>
+<?php $navbar_id = 'navbar-collapse-' . uniqid(); ?>
+<nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#<?php echo $navbar_id; ?>" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="<?php echo $navbar_id; ?>">
+        <?php
+            wp_nav_menu( array(
+                'theme_location'  => 'main',
+                'depth'	          => 2,
+                'container'       => false,
+                'container_class' => false,
+                'container_id'    => false,
+                'menu_class'      => 'navbar-nav mr-auto',
+                'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'          => new WP_Bootstrap_Navwalker(),
+            ) );
+        ?>
+        <?php get_search_form(); ?>
+    </div>
+</nav>
 
 <a href="#fim-menu" id="fim-menu" class="sr-only">Fim do menu</a>
