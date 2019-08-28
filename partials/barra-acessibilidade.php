c7ea0396881b1945379831c74bca3a236d03826a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-28 17:08:26 -0300

    Ajuste dos caracteres especiais

--- partials/barra-acessibilidade.php
+++ partials/barra-acessibilidade.php
@@ -1,9 +1,9 @@
 <nav class="barra-acessibilidade">
     <ul class="barra-acessibilidade__menu">
-        <li><a href="#inicio-conteudo" accesskey="1">Ir para o conteúdo [1]</a></li>
+        <li><a href="#inicio-conteudo" accesskey="1">Ir para o conte&uacute;do [1]</a></li>
         <li><a href="#inicio-menu" accesskey="2">Ir para o menu [2]</a></li>
         <li><a href="#search-field" accesskey="3">Ir para a busca [3]</a></li>
-        <li><a href="#inicio-rodape" accesskey="4">Ir para o rodapé [4]</a></li>
+        <li><a href="#inicio-rodape" accesskey="4">Ir para o rodap&eacute; [4]</a></li>
     </ul>
 
     <ul class="barra-acessibilidade__menu">
