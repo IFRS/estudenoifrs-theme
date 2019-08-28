1f058990083f7a10415c251beaa61a65e2352b27
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-28 17:41:41 -0300

    Remoção de classe que causava problemas

--- inc/widgets.php
+++ inc/widgets.php
@@ -4,7 +4,7 @@ function ingresso_widgets_init() {
 		'name'          => 'Carousel',
 		'id'            => 'area-carousel',
 		'description'   => __('Área para conteúdo em forma de slider, geralmente imagens.', 'ifrs-portal-theme'),
-		'before_widget' => '<div id="%1$s" class="carousel-item active %2$s">',
+		'before_widget' => '<div id="%1$s" class="carousel-item %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<span class="sr-only">',
 		'after_title'   => '</span>',
