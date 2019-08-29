608731a11ffb6bc0a67bbbe72edd5f25c8f72dde
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-29 13:32:40 -0300

    Adição de colunas no footer para facilitar posicionamento

--- footer.php
+++ footer.php
@@ -7,36 +7,42 @@
 
 <footer>
     <div class="container">
-        <div class="footer">
-            <div class="footer-logo">
-                <a href="https://ifrs.edu.br/" data-toggle="tooltip" data-placement="top" title="Portal do IFRS" class="footer-logo__link">
-                    <img class="m-auto img-fluid" data-src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer-marca.png" alt="Marca do IFRS"/>
-                </a>
+        <div class="row footer">
+            <div class="col-12 col-lg-3">
+                <div class="footer-logo">
+                    <a href="https://ifrs.edu.br/" data-toggle="tooltip" data-placement="top" title="Portal do IFRS" class="footer-logo__link">
+                        <img class="m-auto img-fluid" data-src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer-marca.png" alt="Marca do IFRS"/>
+                    </a>
+                </div>
             </div>
-            <?php
-                if (has_nav_menu('main')) {
-                    wp_nav_menu( array(
-                        'theme_location'    => 'main',
-                        'depth'             => 2,
-                        'container'         => 'nav',
-                        'container_class'   => 'site-map',
-                        'container_id'      => false,
-                        'menu_id'           => false,
-                        'menu_class'        => 'site-map__menu',
-                        'fallback_cb'       => false
-                    ));
-                }
-            ?>
-            <address class="endereco">
-                <h2 class="endereco__title">Endere&ccedil;o</h2>
-                <p>Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul</p>
-                <p>
-                    Rua General Os&oacute;rio, 348 | Bairro Centro
-                    <br>
-                    Bento Gon&ccedil;alves/RS | CEP: 95700-086
-                </p>
-                <p>Telefone: (54) 3449-3300</p>
-            </address>
+            <div class="col">
+                <?php
+                    if (has_nav_menu('main')) {
+                        wp_nav_menu( array(
+                            'theme_location'    => 'main',
+                            'depth'             => 2,
+                            'container'         => 'nav',
+                            'container_class'   => 'site-map',
+                            'container_id'      => false,
+                            'menu_id'           => false,
+                            'menu_class'        => 'site-map__menu',
+                            'fallback_cb'       => false
+                        ));
+                    }
+                ?>
+            </div>
+            <div class="col-12 col-md-5 col-lg-4">
+                <address class="endereco">
+                    <h2 class="endereco__title">Endere&ccedil;o</h2>
+                    <p>Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul</p>
+                    <p>
+                        Rua General Os&oacute;rio, 348 | Bairro Centro
+                        <br>
+                        Bento Gon&ccedil;alves/RS | CEP: 95700-086
+                    </p>
+                    <p>Telefone: (54) 3449-3300</p>
+                </address>
+            </div>
         </div>
         <div class="creditos">
             <!-- Wordpress -->
