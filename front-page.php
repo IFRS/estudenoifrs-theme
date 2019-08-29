0330e368fc52cc54c6f50cace8eb0444dd12312e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-29 09:40:19 -0300

    Criação e estilização da área para o texto de introdução

--- front-page.php
+++ front-page.php
@@ -6,4 +6,20 @@
     </div>
 </div>
 
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h2 class="home-title">Estude no <span class="home-title__destaque">#mundo<strong>IFRS</strong></span></h2>
+            <?php if (get_option( 'page_on_front' )) : ?>
+                <div class="home-intro">
+                    <?php
+                        the_post();
+                        the_content();
+                    ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
