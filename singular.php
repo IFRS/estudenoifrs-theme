65215423eead167dec867edf0524de9b1242f835
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-30 15:34:39 -0300

    Criação e estilização do template para páginas e posts

---
+++ singular.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<?php the_post(); ?>
+
+<section class="container">
+    <div class="row">
+        <div class="col-12">
+            <article class="post">
+                <h2 class="post__title"><?php the_title(); ?></h2>
+                <div class="post__content">
+                    <?php
+                        if (has_post_thumbnail()) {
+                            the_post_thumbnail('full', array('class' => 'post__thumb'));
+                        }
+                    ?>
+                    <?php the_content(); ?>
+                </div>
+            </article>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
