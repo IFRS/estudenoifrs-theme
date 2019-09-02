068baa8ce03673e471947baf7c7f8ac76a4dc4d5
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-09-02 17:01:48 -0300

    Estilização do conteúdo principal para combinar com a área de contato

--- singular.php
+++ singular.php
@@ -2,22 +2,24 @@
 
 <?php the_post(); ?>
 
-<section class="container">
-    <div class="row">
-        <div class="col-12">
-            <article class="post">
-                <h2 class="post__title"><?php the_title(); ?></h2>
-                <div class="post__content">
-                    <?php
-                        if (has_post_thumbnail()) {
-                            the_post_thumbnail('full', array('class' => 'post__thumb'));
-                        }
-                    ?>
-                    <?php the_content(); ?>
-                </div>
-            </article>
+<div class="content">
+    <section class="container">
+        <div class="row">
+            <div class="col-12">
+                <article class="post">
+                    <h2 class="post__title"><?php the_title(); ?></h2>
+                    <div class="post__content">
+                        <?php
+                            if (has_post_thumbnail()) {
+                                the_post_thumbnail('full', array('class' => 'post__thumb'));
+                            }
+                        ?>
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+</div>
 
 <?php get_footer(); ?>
