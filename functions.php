646c5252835131bd2b69bd61f83667c6b1d207a0
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-03-26 15:30:45 -0300

    Remoção de arquivos não mais utilizados

--- functions.php
+++ functions.php
@@ -11,11 +11,7 @@ require_once('inc/permissions.php');
 // Configurações para o tema
 require_once('inc/theme-config.php');
 
-// Registro de Sidebars
-require_once('inc/widgets.php');
-
 // Tamanho do resumo e resumo em páginas
-require_once('inc/page-excerpt.php');
 require_once('inc/excerpt.php');
 
 // Breadcrumb
@@ -26,9 +22,6 @@ require_once('inc/pagination.php');
 
 // Controla a busca com termo vazio
 require_once('inc/empty-search.php');
-
-// Script Condicional
-require_once('inc/script_conditional.php');
 
 // Scripts & Styles
 require_once('inc/assets.php');
