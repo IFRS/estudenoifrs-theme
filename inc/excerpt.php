b791770d63c500336186339909d7fcd49db84d02
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-03-26 15:28:13 -0300

    União de arquivos para melhor organização

--- inc/excerpt.php
+++ inc/excerpt.php
@@ -1,4 +1,8 @@
 <?php
+add_action( 'init', function() {
+    add_post_type_support( 'page', 'excerpt' );
+}, 999 );
+
 add_filter( 'excerpt_length', function($length) {
     return 20;
 }, 999 );
--- inc/theme-config.php
+++ inc/theme-config.php
@@ -4,7 +4,7 @@ register_nav_menus(
     array(
         'main' => 'Menu Principal',
     )
-    );
+);
 
 // Habilita a personalização de cabeçalho
 add_theme_support('custom-header', array(
@@ -24,3 +24,25 @@ add_theme_support('custom-header', array(
 
 // Habilita imagens destaque
 add_theme_support( 'post-thumbnails' );
+
+// Registra as áreas para widgets
+add_action('widgets_init', function() {
+    register_sidebar(array(
+        'name'          => 'Carousel',
+        'id'            => 'area-carousel',
+        'description'   => __('Área para conteúdo em forma de slider, geralmente imagens.', 'ifrs-ingresso-theme'),
+        'before_widget' => '<div id="%1$s" class="carousel-item %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<span class="sr-only">',
+        'after_title'   => '</span>',
+    ));
+    register_sidebar(array(
+        'name'          => 'Contato',
+        'id'            => 'area-contato',
+        'description'   => __('Área para formulário de contato no rodapé, aparece em todas as páginas.', 'ifrs-ingresso-theme'),
+        'before_widget' => '<div id="%1$s" class="contato__widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<span class="sr-only">',
+        'after_title'   => '</span>',
+    ));
+});
