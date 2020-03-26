aa70bd3a0709f630f8aa40c88d5e8cd4b7f63d87
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-03-26 15:25:35 -0300

    Adição de aba com todas as oportunidades

--- front-page.php
+++ front-page.php
@@ -30,12 +30,19 @@
                     'taxonomy' => 'forma',
                     'hide_empty' => false,
                 ));
+                array_unshift($formas, 0);
             ?>
             <ul class="nav nav-pills justify-content-center my-3" role="tablist">
                 <?php foreach ($formas as $forma) : ?>
-                    <li class="nav-item mx-3">
-                        <button class="nav-link btn btn-forma" type="button" data-toggle="pill" data-target="#tab-forma-<?php echo $forma->term_id; ?>" role="tab" aria-controls="collapse-forma-<?php echo $forma->term_id; ?>" aria-selected="false"><?php echo $forma->name; ?></button>
-                    </li>
+                    <?php if ($forma) : ?>
+                        <li class="nav-item mx-3">
+                            <button class="nav-link btn btn-forma" type="button" data-toggle="pill" data-target="#tab-forma-<?php echo $forma->term_id; ?>" role="tab" aria-controls="collapse-forma-<?php echo $forma->term_id; ?>" aria-selected="false"><?php echo $forma->name; ?></button>
+                        </li>
+                    <?php else : ?>
+                        <li class="nav-item mx-3">
+                            <button class="nav-link btn btn-forma active" type="button" data-toggle="pill" data-target="#tab-forma-todas" role="tab" aria-controls="collapse-forma-todas" aria-selected="true">Tudo</button>
+                        </li>
+                    <?php endif; ?>
                 <?php endforeach; ?>
             </ul>
         </div>
@@ -49,21 +56,23 @@
                 <div class="tab-content">
                     <?php foreach ($formas as $forma) : ?>
                         <?php
+                            $tax_query = array();
+                            if ($forma) {
+                                $tax_query[] = array(
+                                    'taxonomy' => 'forma',
+                                    'terms' => $forma->term_id,
+                                );
+                            }
                             $args = array(
                                 'post_type' => 'oportunidade',
                                 'nopaging' => true,
                                 'posts_per_page' => -1,
-                                'tax_query' => array(
-                                    array(
-                                        'taxonomy' => 'forma',
-                                        'terms' => $forma->term_id,
-                                    ),
-                                ),
+                                'tax_query' => $tax_query,
                             );
 
                             $oportunidades = new WP_Query($args);
                         ?>
-                        <div class="tab-pane fade" id="tab-forma-<?php echo $forma->term_id; ?>">
+                        <div class="tab-pane fade<?php echo (!$forma ? ' active show' : ''); ?>" id="tab-forma-<?php echo ($forma ? $forma->term_id : 'todas'); ?>">
                             <?php if ($oportunidades->have_posts()) : ?>
                                 <div class="oportunidades">
                                     <?php while ($oportunidades->have_posts()) : $oportunidades->the_post(); ?>
@@ -117,7 +126,7 @@
                                 </div>
                             <?php else : ?>
                                 <div class="alert alert-info" role="alert">
-                                    Não existem oportunidades para essa forma de ingresso no momento. Fique atento para novas publicações.
+                                    N&atilde;o existem oportunidades para essa forma de ingresso no momento. Fique atento para novas publica&ccedil;&otilde;es.
                                 </div>
                             <?php endif; ?>
                             <?php wp_reset_postdata(); ?>
