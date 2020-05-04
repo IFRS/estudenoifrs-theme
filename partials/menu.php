2f22da2bafba8170e542a2cb8bdc7b157524be93
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-05-04 17:26:10 -0300

    Correção em referências e textos do menu

--- partials/menu.php
+++ partials/menu.php
@@ -2,8 +2,8 @@
 
 <?php $navbar_id = 'navbar-collapse-' . uniqid(); ?>
 <nav class="navbar navbar-expand-md navbar-light" role="navigation">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#<?php echo $navbar_id; ?>" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span> Menu
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#<?php echo $navbar_id; ?>" aria-controls="<?php echo $navbar_id; ?>" aria-expanded="false" aria-label="<?php _e('Alternar menu'); ?>">
+        <span class="navbar-toggler-icon"></span>&nbsp;Menu
     </button>
     <div class="collapse navbar-collapse" id="<?php echo $navbar_id; ?>">
         <?php
