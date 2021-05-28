c0627a50559aa69125f9487e9ed95d755294bf61
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-05-28 16:18:05 -0300

    Remoção do navwalker para Bootstrap 4

--- functions.php
+++ functions.php
@@ -26,15 +26,6 @@ require_once('inc/empty-search.php');
 // Scripts & Styles
 require_once('inc/assets.php');
 
-// Bootstrap 4 navwalker
-if ( ! file_exists( get_stylesheet_directory() . '/vendor/wp-bootstrap/wp-bootstrap-navwalker/class-wp-bootstrap-navwalker.php' ) ) {
-	// file does not exist... return an error.
-	return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
-} else {
-	// file exists... require it.
-	require_once get_stylesheet_directory() . '/vendor/wp-bootstrap/wp-bootstrap-navwalker/class-wp-bootstrap-navwalker.php';
-}
-
 // Custom Post Type
 require('inc/cpt/pergunta.php');
 
