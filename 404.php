9607cb51260cb51bafecedbfcf16f5aef2745810
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-05-31 13:36:36 -0300

    Ajuste no alert da página de erro 404

--- 404.php
+++ 404.php
@@ -2,9 +2,9 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-12 col-lg-8 offset-lg-2">
-            <div class="alert alert-danger" role="alert">
-                <p><strong>Ops!&nbsp;</strong>A p&aacute;gina que voc&ecirc; procura n&atilde;o foi encontrada. Tente voltar para a <a href="<?php echo esc_url(home_url()); ?>">p&aacute;gina inicial</a>.</p>
+        <div class="col-12 col-lg-6 offset-lg-3">
+            <div class="alert alert-danger mb-5" role="alert">
+                <strong>Ops!&nbsp;</strong>A p&aacute;gina que voc&ecirc; procura n&atilde;o foi encontrada. Tente voltar para a <a href="<?php echo esc_url(home_url()); ?>">p&aacute;gina inicial</a>.
             </div>
         </div>
     </div>
