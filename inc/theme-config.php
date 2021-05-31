e0690b4db4eb6cc24250db2ea416444a703ab1d3
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-05-31 09:25:33 -0300

    Habilitação de várias funções para o tema

--- inc/theme-config.php
+++ inc/theme-config.php
@@ -1,4 +1,27 @@
 <?php
+// Remove Gutenberg custom options
+add_theme_support( 'editor-color-palette' );
+add_theme_support( 'editor-gradient-presets' );
+add_theme_support( 'disable-custom-colors' );
+add_theme_support( 'disable-custom-gradients' );
+add_theme_support( 'disable-custom-font-sizes' );
+add_theme_support( 'custom-units', array() );
+
+// Add theme support for Automatic Feed Links
+add_theme_support( 'automatic-feed-links' );
+
+// Add theme support for Featured Images
+add_theme_support( 'post-thumbnails' );
+
+// Add theme support for HTML5 Semantic Markup
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+
+// Add theme support for Responsive Embeds
+add_theme_support( 'responsive-embeds' );
+
+// Add theme support for document <title> tag
+add_theme_support( 'title-tag' );
+
 // Habilita a personalização do logo
 add_theme_support( 'custom-logo', array(
     'width'       => 540,
@@ -6,9 +29,6 @@ add_theme_support( 'custom-logo', array(
     'flex-height' => false,
     'flex-width'  => true,
 ) );
-
-// Habilita imagens destaque
-add_theme_support( 'post-thumbnails' );
 
 // Registra as áreas para widgets
 add_action('widgets_init', function() {
