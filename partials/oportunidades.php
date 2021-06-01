f6d34efeaab8a50cf113c05e04b1cb666e3a227c
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-06-01 17:51:50 -0300

    Ajuste no texto para lista vazia de oportunidades

--- partials/oportunidades.php
+++ partials/oportunidades.php
@@ -107,7 +107,7 @@
                                 </div>
                             <?php else : ?>
                                 <div class="alert alert-info" role="alert">
-                                    N&atilde;o existem oportunidades para essa forma de ingresso no momento. Fique atento para novas publica&ccedil;&otilde;es.
+                                    N&atilde;o existem oportunidades para essa forma de ingresso<?php echo (is_tax('unidade')) ? ' e unidade ' : ' '; ?>no momento. Fique atento para novas publica&ccedil;&otilde;es.
                                 </div>
                             <?php endif; ?>
                             <?php wp_reset_postdata(); ?>
