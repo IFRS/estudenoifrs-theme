00af966ce987326e875c2ad9268e9c810bb4d7e2
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-06-09 16:04:01 -0300

    Posicionamento das oportunidades abertas através de JS

--- src/oportunidades.js
+++ src/oportunidades.js
@@ -1,14 +1,31 @@
 $(function() {
     $('.oportunidade__btn-toggle').on('click', function(e) {
-        if ($(this).closest('.oportunidade').hasClass('oportunidade--open')) {
-            $(this).closest('.oportunidade').fadeOut(500, function() {
+        const oportunidades = $(this).closest('.oportunidades');
+        const oportunidade = $(this).closest('.oportunidade');
+
+        const colCount = oportunidades.css('grid-template-columns').split(' ').length;
+        const rowPosition = Math.floor(oportunidade.index() / colCount);
+        const colPosition = oportunidade.index() % colCount;
+
+        if (oportunidade.hasClass('oportunidade--open')) {
+            oportunidade.fadeOut(500, function() {
                 $(this).removeClass('oportunidade--open').show();
+                oportunidade.removeAttr('style');
             });
         } else {
-            $(this).closest('.oportunidades').children('.oportunidade--open').removeClass('oportunidade--open');
-            $(this).closest('.oportunidade').addClass('oportunidade--open').hide().fadeIn(750);
+            oportunidades.children('.oportunidade--open').removeClass('oportunidade--open').removeAttr('style');
+
+            if (colPosition < colCount - 1) {
+                oportunidade.css('grid-column', (colPosition + 1) + ' / span 2');
+            } else if (colPosition == colCount - 1) {
+                oportunidade.css('grid-column', (colCount - 1) + ' / span 2');
+            }
+
+            oportunidade.css('grid-row', (rowPosition + 1) + '/ span 3')
+
+            oportunidade.addClass('oportunidade--open').hide().fadeIn(750);
             $('html, body').animate({
-                scrollTop: $(this).offset().top - 30
+                scrollTop: $(this).offset().top - 32
             }, 250);
         }
     });
