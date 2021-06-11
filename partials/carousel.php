44f5880f1392ceab1d62dd13c206154a803682ca
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-06-11 16:05:36 -0300

    Ajustes no carrossel

--- partials/carousel.php
+++ partials/carousel.php
@@ -1,25 +1,25 @@
 <?php if (is_active_sidebar('area-carousel')) : ?>
-    <?php $carousel_id = 'carousel-' . uniqid(); ?>
+    <?php $carousel_id = uniqid('carousel-'); ?>
     <div id="<?php echo $carousel_id; ?>" class="carousel slide carousel-fade" data-bs-ride="carousel">
         <ol class="carousel-indicators">
             <?php
                 $total_widgets = wp_get_sidebars_widgets();
-                $sidebar_widgets = count($total_widgets['area-carousel']);
+                $carousel_widgets = count($total_widgets['area-carousel']);
             ?>
-            <?php for ($i = 0; $i < $sidebar_widgets; $i++) : ?>
+            <?php for ($i = 0; $i < $carousel_widgets; $i++) : ?>
                 <li data-bs-target="#<?php echo $carousel_id; ?>" data-bs-slide-to="<?php echo $i; ?>" <?php echo ($i == 0) ? 'class="active"' : ''; ?>></li>
             <?php endfor; ?>
         </ol>
         <div class="carousel-inner">
             <?php dynamic_sidebar('area-carousel'); ?>
         </div>
-        <a class="carousel-control-prev" href="#<?php echo $carousel_id; ?>" role="button" data-bs-slide="prev">
+        <button class="carousel-control-prev" data-bs-target="#<?php echo $carousel_id; ?>" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Anterior</span>
-        </a>
-        <a class="carousel-control-next" href="#<?php echo $carousel_id; ?>" role="button" data-bs-slide="next">
+            <span class="visually-hidden">Slide Anterior</span>
+        </button>
+        <button class="carousel-control-next" data-bs-target="#<?php echo $carousel_id; ?>" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Pr&oacute;ximo</span>
-        </a>
+            <span class="visually-hidden">Próximo Slide</span>
+        </button>
     </div>
 <?php endif; ?>
