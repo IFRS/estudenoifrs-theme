c9cde5919e10f17a406971fb3d97f318982af726
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-06-16 14:37:34 -0300

    Ajuste na inicialização da lightgallery para galerias do WP

--- src/_lightgallery.js
+++ src/_lightgallery.js
@@ -1,10 +1,19 @@
 import lightGallery from 'lightgallery';
 
 document.addEventListener('DOMContentLoaded', function() {
-    let images = document.querySelectorAll('a[href$=".jpg"],a[href$=".jpeg"],a[href$=".png"],a[href$=".gif"],a[href$=".svg"]');
-    images.forEach(function(el) {
-        lightGallery(el, {
-            selector: 'this',
+    let galleries = document.querySelectorAll('.wp-block-gallery');
+    galleries.forEach(function(gallery) {
+        lightGallery(gallery, {
+            selector: '.blocks-gallery-item a',
         });
+    });
+
+    let images = document.querySelectorAll('a[href$=".jpg"],a[href$=".jpeg"],a[href$=".png"],a[href$=".gif"],a[href$=".svg"]');
+    images.forEach(function(image) {
+        if (!image.closest('.wp-block-gallery')) {
+            lightGallery(image, {
+                selector: 'this',
+            });
+        }
     });
 });
