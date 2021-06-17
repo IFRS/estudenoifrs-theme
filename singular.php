3a26178356e660f40a4d1acece463cfb060009da
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-06-17 14:42:57 -0300

    Remoção de marcação desnecessária

--- singular.php
+++ singular.php
@@ -4,30 +4,26 @@
 
 <div class="content">
     <section class="container">
-        <div class="row">
-            <div class="col-12">
-                <article class="post">
-                    <h2 class="post__title"><?php the_title(); ?></h2>
-                    <div class="post__content">
-                        <?php
-                            if (has_post_thumbnail()) {
-                                the_post_thumbnail('full', array('class' => 'post__thumb'));
-                            }
-                        ?>
-                        <?php the_content(); ?>
-                        <nav aria-label="Paginação do Conteúdo">
-                            <?php
-                                wp_link_pages(array(
-                                    'before' => '<ul class="pagination"><li class="page-item">',
-                                    'separator' => '</li><li class="page-item">',
-                                    'after'  => '</li></ul>',
-                                ));
-                            ?>
-                        </nav>
-                    </div>
-                </article>
+        <article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
+            <h2 class="post__title"><?php the_title(); ?></h2>
+            <div class="post__content">
+                <?php
+                    if (has_post_thumbnail()) {
+                        the_post_thumbnail('full', array('class' => 'post__thumb'));
+                    }
+                ?>
+                <?php the_content(); ?>
+                <nav aria-label="Paginação do Conteúdo">
+                    <?php
+                        wp_link_pages(array(
+                            'before' => '<ul class="pagination"><li class="page-item">',
+                            'separator' => '</li><li class="page-item">',
+                            'after'  => '</li></ul>',
+                        ));
+                    ?>
+                </nav>
             </div>
-        </div>
+        </article>
     </section>
 </div>
 
