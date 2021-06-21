b9fa8aa155a5994619e339ad449db734d4e7d128
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-06-21 16:13:18 -0300

    Alteração na forma de pegar as oportunidades para adicionar contador

--- partials/oportunidades.php
+++ partials/oportunidades.php
@@ -7,20 +7,54 @@
             'taxonomy' => 'forma',
             'hide_empty' => false,
         ));
-        array_unshift($formas, 0);
+        array_unshift($formas, new stdClass());
     ?>
     <ul class="nav nav-pills justify-content-center mt-3 mb-1 mx-auto forma-list" role="tablist">
-        <?php foreach ($formas as $forma) : ?>
-            <?php if ($forma) : ?>
+        <?php foreach ($formas as &$forma) : ?>
+            <?php
+                $today = new Datetime("now - 1 day");
+
+                $tax_query = array();
+
+                if (!empty($forma->term_id)) {
+                    $tax_query[] = array(
+                        'taxonomy' => 'forma',
+                        'terms' => $forma->term_id,
+                    );
+                }
+
+                if (is_tax('unidade')) {
+                    $tax_query[] = array(
+                        'taxonomy' => 'unidade',
+                        'terms' => get_queried_object()->term_id,
+                    );
+                }
+
+                $args = array(
+                    'post_type' => 'oportunidade',
+                    'nopaging' => true,
+                    'posts_per_page' => -1,
+                    'tax_query' => $tax_query,
+                    'meta_key' => '_oportunidade_inscricao_termino',
+                    'meta_compare' => '>=',
+                    'meta_value' => $today->format('U'),
+                    'orderby' => 'meta_value_num',
+                    'order' => 'ASC',
+                );
+
+                $forma->oportunidades = new WP_Query($args);
+            ?>
+            <?php if (!empty($forma->term_id)) : ?>
                 <li class="nav-item mx-1 my-1">
-                    <button class="nav-link" type="button" data-bs-toggle="pill" data-bs-target="#tab-forma-<?php echo $forma->term_id; ?>" role="tab" aria-controls="collapse-forma-<?php echo $forma->term_id; ?>" aria-selected="false"><?php echo $forma->name; ?></button>
+                    <button class="nav-link" type="button" data-bs-toggle="pill" data-bs-target="#tab-forma-<?php echo $forma->term_id; ?>" role="tab" aria-controls="collapse-forma-<?php echo $forma->term_id; ?>" aria-selected="false"><?php echo $forma->name; ?>&nbsp;&nbsp;<span class="badge"><?php echo $forma->oportunidades->post_count; ?></span></button>
                 </li>
             <?php else : ?>
                 <li class="nav-item mx-1 my-1">
-                    <button class="nav-link active" type="button" data-bs-toggle="pill" data-bs-target="#tab-forma-todas" role="tab" aria-controls="collapse-forma-todas" aria-selected="true">Tudo</button>
+                    <button class="nav-link active" type="button" data-bs-toggle="pill" data-bs-target="#tab-forma-todas" role="tab" aria-controls="collapse-forma-todas" aria-selected="true">Tudo&nbsp;&nbsp;<span class="badge"><?php echo $forma->oportunidades->post_count; ?></span></button>
                 </li>
             <?php endif; ?>
         <?php endforeach; ?>
+        <?php unset($forma); ?>
     </ul>
 </section>
 
@@ -56,48 +90,15 @@
         </div>
         <div class="tab-content" aria-live="polite">
             <?php foreach ($formas as $forma) : ?>
-                <?php
-                    $today = new Datetime("now - 1 day");
-
-                    $tax_query = array();
-
-                    if ($forma) {
-                        $tax_query[] = array(
-                            'taxonomy' => 'forma',
-                            'terms' => $forma->term_id,
-                        );
-                    }
-
-                    if (is_tax('unidade')) {
-                        $tax_query[] = array(
-                            'taxonomy' => 'unidade',
-                            'terms' => get_queried_object()->term_id,
-                        );
-                    }
-
-                    $args = array(
-                        'post_type' => 'oportunidade',
-                        'nopaging' => true,
-                        'posts_per_page' => -1,
-                        'tax_query' => $tax_query,
-                        'meta_key' => '_oportunidade_inscricao_termino',
-                        'meta_compare' => '>=',
-                        'meta_value' => $today->format('U'),
-                        'orderby' => 'meta_value_num',
-                        'order' => 'ASC',
-                    );
-
-                    $oportunidades = new WP_Query($args);
-                ?>
-                <div class="tab-pane fade<?php echo (!$forma ? ' active show' : ''); ?>" id="tab-forma-<?php echo ($forma ? $forma->term_id : 'todas'); ?>" role="tabpanel">
+                <div class="tab-pane fade<?php echo (empty($forma->term_id) ? ' active show' : ''); ?>" id="tab-forma-<?php echo (!empty($forma->term_id) ? $forma->term_id : 'todas'); ?>" role="tabpanel">
                     <?php if (!empty($forma->description)) : ?>
                         <div class="forma-description">
                             <?php echo wpautop($forma->description); ?>
                         </div>
                     <?php endif; ?>
-                    <?php if ($oportunidades->have_posts()) : ?>
+                    <?php if ($forma->oportunidades->have_posts()) : ?>
                         <div class="oportunidades">
-                            <?php while ($oportunidades->have_posts()) : $oportunidades->the_post(); ?>
+                            <?php while ($forma->oportunidades->have_posts()) : $forma->oportunidades->the_post(); ?>
                                 <?php echo get_template_part('partials/oportunidade'); ?>
                             <?php endwhile; ?>
                         </div>
