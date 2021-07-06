dcd94673e706ebf78acf8109fdd676ae04e3f4b8
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-07-06 13:54:01 -0300

    Adição de thumbnails nas galerias

--- src/_lightgallery.js
+++ src/_lightgallery.js
@@ -1,10 +1,21 @@
 import lightGallery from 'lightgallery';
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
 
 document.addEventListener('DOMContentLoaded', function() {
     let galleries = document.querySelectorAll('.wp-block-gallery');
     galleries.forEach(function(gallery) {
+        gallery.querySelectorAll('a').forEach(function(link) {
+            let img = link.querySelector('img');
+
+            if (img) {
+                let src = img.getAttribute('data-src');
+                link.setAttribute('data-thumb', src);
+            }
+        });
         lightGallery(gallery, {
+            plugins: [lgThumbnail],
             selector: '.blocks-gallery-item a',
+            exThumbImage: 'data-thumb',
         });
     });
 
