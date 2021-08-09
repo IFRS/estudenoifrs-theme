dae8fb1316fba5ace1c6b8b09fabd8fc20f831b5
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-08-09 12:24:05 -0300

    Remoção do template de busca para poder usar buscar locais

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
 <?php get_header(); ?>
 
 <section class="container">
-    <div class="alert alert-danger" role="alert">
-        <strong>Erro!</strong> Template n&atilde;o encontrado. Tente voltar para a <a href="<?php echo esc_url(home_url()); ?>">p&aacute;gina inicial</a>.
-    </div>
+    <?php if (is_search()) : ?>
+        <div class="row">
+            <div class="col-12">
+                <h2 class="search__title">Resultados da busca por &quot;<?php the_search_query(); ?>&quot;</h2>
+            </div>
+        </div>
+
+        <?php if (!have_posts()) : ?>
+            <div class="alert alert-warning" role="alert">
+                <strong>Ops!</strong> Nenhum resultado encontrado para o termo &quot;<?php the_search_query(); ?>&quot;.
+            </div>
+        <?php else: ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <div class="row">
+                    <div class="col-12">
+                        <article class="search__entry">
+                            <h3 class="search__entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                            <div class="search__entry-summary">
+                                <?php the_excerpt(); ?>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <hr>
+            <?php endwhile; ?>
+            <?php echo ifrs_ingresso_pagination(); ?>
+        <?php endif; ?>
+    <?php else : ?>
+        <div class="alert alert-danger" role="alert">
+            <strong>Erro!</strong> Template n&atilde;o encontrado. Tente voltar para a <a href="<?php echo esc_url(home_url()); ?>">p&aacute;gina inicial</a>.
+        </div>
+    <?php endif; ?>
 </section>
 
 <?php get_footer(); ?>
