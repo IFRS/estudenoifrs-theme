ad218ef44d6b00623d5d7d414bd1d9b19c74a881
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-08-20 15:03:14 -0300

    Melhor organização das classes para dados do curso

--- partials/curso-item.php
+++ partials/curso-item.php
@@ -18,18 +18,18 @@
     <hr class="curso-item__separador">
     <h4 class="curso-item__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
     <p class="curso-item__meta">
-        <span class="curso-item__cargahoraria">
+        <span class="curso-item__meta--cargahoraria">
             <?php echo esc_html(get_post_meta( get_the_ID(), '_curso_duracao', true )); ?> (<?php echo esc_html(get_post_meta( get_the_ID(), '_curso_carga_horaria', true )); ?>h)
         </span>
         <br>
-        <span class="curso-item__turnos">
+        <span class="curso-item__meta--turnos">
             <?php $turnos = wp_get_post_terms(get_the_ID(), 'turno', array('orderby' => 'term_order')); ?>
             <?php foreach ($turnos as $turno) : ?>
                 <?php echo $turno->name; echo ($turno !== end($turnos)) ? ', ' : ''; ?>
             <?php endforeach; ?>
         </span>
         <br>
-        <span class="curso-item__modalidades">
+        <span class="curso-item__meta--modalidades">
             <?php foreach (get_the_terms(get_the_ID(), 'modalidade') as $modalidade) : ?>
                 <?php echo $modalidade->name; ?>
             <?php endforeach; ?>
