e55c8f97ebf8f8ae0f9ae6c050e150f173be800b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-09-03 16:49:57 -0300

    Ajuste no efeito do carrossel

--- partials/carousel.php
+++ partials/carousel.php
@@ -1,6 +1,6 @@
 <?php if (is_active_sidebar('area-carousel')) : ?>
     <?php $carousel_id = uniqid('carousel-'); ?>
-    <div id="<?php echo $carousel_id; ?>" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div id="<?php echo $carousel_id; ?>" class="carousel slide" data-bs-ride="carousel">
         <ol class="carousel-indicators">
             <?php
                 $total_widgets = wp_get_sidebars_widgets();
