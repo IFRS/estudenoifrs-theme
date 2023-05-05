8259794cba3688024d813963f2a4bdf4e00b953a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2023-05-05 10:56:37 -0300

    Adição de verificações para que os espaços do meta dos Cursos fique correto

--- partials/curso-item.php
+++ partials/curso-item.php
@@ -18,19 +18,20 @@
     <hr class="curso-item__separador">
     <h4 class="curso-item__title"><a href="<?php the_permalink(); ?>" style="color: <?php echo end($cores); ?>" class="stretched-link"><?php the_title(); ?></a></h4>
     <p class="curso-item__meta">
-        <span class="curso-item__meta--cargahoraria">
-            <?php
-                $duracao = get_post_meta( get_the_ID(), '_curso_duracao', true );
-                $cargahoraria = get_post_meta( get_the_ID(), '_curso_carga_horaria', true );
-            ?>
-            <?php if ($duracao) : ?>
-                <?php echo esc_html($duracao); ?> (<?php echo esc_html($cargahoraria); ?>h)
-            <?php else : ?>
-                <?php echo esc_html($cargahoraria); ?>h
-            <?php endif; ?>
-        </span>
-
-        <br>
+        <?php
+            $duracao = get_post_meta( get_the_ID(), '_curso_duracao', true );
+            $cargahoraria = get_post_meta( get_the_ID(), '_curso_carga_horaria', true );
+        ?>
+        <?php if ($duracao || $cargahoraria) : ?>
+            <span class="curso-item__meta--cargahoraria">
+                <?php if ($duracao) : ?>
+                    <?php echo esc_html($duracao); ?> (<?php echo esc_html($cargahoraria); ?>h)
+                <?php else : ?>
+                    <?php echo esc_html($cargahoraria); ?>h
+                <?php endif; ?>
+            </span>
+            <br>
+        <?php endif; ?>
 
         <?php
             $turnos_ordenados = get_terms(array(
@@ -64,16 +65,14 @@
 
             $turnos = ifrs_sortArrayByArrayValues($turnos, $turnos_ordenados);
         ?>
-
         <?php if ($turnos) : ?>
             <span class="curso-item__meta--turnos">
                 <?php foreach ($turnos as $turno_id) : ?>
                     <?php echo get_term($turno_id)->name; echo ($turno_id !== end($turnos)) ? ', ' : ''; ?>
                 <?php endforeach; ?>
             </span>
+            <br>
         <?php endif; ?>
-
-        <br>
 
         <?php $modalidades = get_the_terms(get_the_ID(), 'modalidade'); ?>
         <?php if ($modalidades) : ?>
