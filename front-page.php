b124b99cbac514a34fb8f5bd75dd5c60a8f2f1c7
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-06-17 17:07:16 -0300

    Remoção de conteúdo da página inicial, e habilitação de mais opções de personalização

--- front-page.php
+++ front-page.php
@@ -2,22 +2,9 @@
 
 <section class="container">
     <article class="front-page">
-        <div class="row align-items-center">
-            <div class="col col-md-7 col-lg-8 col-xl-9 d-none d-md-block">
-                <div class="front-page__text">
-                    <?php if (get_option('page_on_front')) : ?>
-                        <?php
-                            the_post();
-                            the_content();
-                        ?>
-                    <?php endif; ?>
-                </div>
-            </div>
-            <div class="col text-center">
-                <a href="<?php echo get_post_type_archive_link('curso'); ?>" class="front-page__banner">
-                    <img src="<?php echo get_parent_theme_file_uri( '/img/guia-de-cursos.png' ); ?>" class="img-fluid" alt="Guia de Cursos" loading="lazy">
-                </a>
-            </div>
+        <?php if (get_option('page_on_front')) : ?>
+            <?php the_content(); ?>
+        <?php endif; ?>
     </article>
 </section>
 
