757015e9145aed7ebd6fd73c0512c722dc037adb
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-06-18 11:46:12 -0300

    Ajustes no pattern de card, deixando o layout mais fluído e usando uma sombra leve

--- patterns/card.php
+++ patterns/card.php
@@ -1,26 +1,26 @@
 <?php
 /**
  * Title: Card
- * Slug: themeslug/hero
+ * Slug: ifrs/card
  * Categories: featured,banner,call-to-action
  * Post Types: page
  */
 ?>
 
-<!-- wp:group {"style":{"border":{"radius":"1rem"}},"backgroundColor":"gray-100","layout":{"type":"default"}} -->
-<div class="wp-block-group has-gray-100-background-color has-background" style="border-radius:1rem">
+<!-- wp:group {"style":{"border":{"radius":"1rem"},"dimensions":{"minHeight":"100%"}},"shadow":"var:preset|shadow|light"},"backgroundColor":"gray-100","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-gray-100-background-color has-background" style="border-radius:1rem;min-height:100%">
   <!-- wp:heading -->
   <h3 class="wp-block-heading"><span class="wp-rich-text-font-awesome-icon wp-font-awesome-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="font-awesome" class="svg-inline--fa fa-font-awesome " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M91.7 96C106.3 86.8 116 70.5 116 52C116 23.3 92.7 0 64 0S12 23.3 12 52c0 16.7 7.8 31.5 20 41l0 3 0 352 0 64 64 0 0-64 373.6 0c14.6 0 26.4-11.8 26.4-26.4c0-3.7-.8-7.3-2.3-10.7L432 272l61.7-138.9c1.5-3.4 2.3-7 2.3-10.7c0-14.6-11.8-26.4-26.4-26.4L91.7 96z"></path></svg></span> Título</h3>
   <!-- /wp:heading -->
 
   <!-- wp:paragraph -->
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum eros id metus rutrum pharetra blandit semper turpis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse euismod luctus sodales. Vivamus dolor tellus, condimentum et bibendum et, bibendum eget augue. Sed tortor ante, varius at porttitor vel, fringilla vitae ex. Donec varius dolor nec metus porttitor tempor. Proin vulputate a tortor ut tristique. Pellentesque tristique blandit tristique.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum eros id metus rutrum pharetra blandit semper turpis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse euismod luctus sodales.</p>
   <!-- /wp:paragraph -->
 
-  <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+  <!-- wp:buttons {"style":{"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","justifyContent":"right","verticalAlignment":"bottom","flexWrap":"wrap","orientation":"horizontal"}} -->
   <div class="wp-block-buttons">
     <!-- wp:button -->
-    <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Chamada para Ação</a></div>
+    <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Saiba mais</a></div>
     <!-- /wp:button -->
   </div>
   <!-- /wp:buttons -->
