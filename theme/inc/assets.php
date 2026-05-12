aaedd56fd98d1ac37b7c15f8bf8ce3dd77393f8d
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-05-12 17:50:03 -0300

    Mudança dos scripts para módulos

--- theme/inc/assets.php
+++ theme/inc/assets.php
@@ -6,9 +6,9 @@ if (file_exists($manifestFile)) {
 
     /* Block Editor Assets */
     add_action('enqueue_block_editor_assets', function () use ($manifest) {
-        /* wp_enqueue_style( $handle, $src, $deps, $ver, $media ); */
+        /* wp_enqueue_style( string $handle, string $src, string[] $deps = array(), string|bool|null $ver = false, string $media ) */
 
-        wp_enqueue_style( $manifest['sass/estude-editor.scss']['name'], get_parent_theme_file_uri($manifest['sass/estude-editor.scss']['file']), array(), null, 'all');
+        wp_enqueue_style( $manifest['sass/estude-editor.scss']['name'], get_parent_theme_file_uri($manifest['sass/estude-editor.scss']['file']), array(), false, 'all');
     });
 
     /* Fonts Preload */
@@ -19,30 +19,32 @@ if (file_exists($manifestFile)) {
 
     /* Frontend Styles and Scripts */
     add_action( 'wp_enqueue_scripts', function() use ($manifest) {
-        /* wp_enqueue_style( $handle, $src, $deps, $ver, $media ); */
+        /* wp_register_style( string $handle, string|false $src, string[] $deps = array(), string|bool|null $ver = false, string $media ): bool */
+        /* wp_enqueue_style( string $handle, string $src, string[] $deps = array(), string|bool|null $ver = false, string $media ) */
 
-        wp_enqueue_style( $manifest['sass/fonts.scss']['name'], get_parent_theme_file_uri($manifest['sass/fonts.scss']['file']), array(), null, 'all' );
+        wp_enqueue_style( $manifest['sass/fonts.scss']['name'], get_parent_theme_file_uri($manifest['sass/fonts.scss']['file']), array(), false, 'all' );
 
-        wp_enqueue_style( $manifest['sass/vendor.scss']['name'], get_parent_theme_file_uri($manifest['sass/vendor.scss']['file']), array(), null, 'all' );
+        wp_enqueue_style( $manifest['sass/vendor.scss']['name'], get_parent_theme_file_uri($manifest['sass/vendor.scss']['file']), array(), false, 'all' );
 
-        wp_enqueue_style( $manifest['sass/estude.scss']['name'], get_parent_theme_file_uri($manifest['sass/estude.scss']['file']), array(), null, 'all' );
+        wp_enqueue_style( $manifest['sass/estude.scss']['name'], get_parent_theme_file_uri($manifest['sass/estude.scss']['file']), array(), false, 'all' );
 
-        /* wp_register_script( $handle, $src, $deps, $ver, $in_footer ); */
-        /* wp_enqueue_script( $handle[, $src, $deps, $ver, $in_footer] ); */
+        /* wp_register_script( string $handle, string|false $src, string[] $deps = array(), string|bool|null $ver = false, array|bool $args = array() ): bool */
+        /* wp_enqueue_script( string $handle, string $src, string[] $deps = array(), string|bool|null $ver = false, array|bool $args = array() ) */
+        /* wp_enqueue_script_module( string $id, string $src, array $deps = array(), string|false|null $version = false, array $args = array() ) */
 
-        wp_enqueue_script( $manifest['src/estude.js']['name'], get_parent_theme_file_uri( $manifest['src/estude.js']['file'] ), array(), null, true );
+        wp_enqueue_script_module( $manifest['src/estude.js']['name'], get_parent_theme_file_uri( $manifest['src/estude.js']['file'] ), array(), false, array( 'in_footer' => true ) );
 
         if (is_post_type_archive( 'oportunidade' ) || is_tax( 'unidade' ) || is_front_page()) {
-            wp_enqueue_script( $manifest['src/oportunidades.js']['name'], get_parent_theme_file_uri( $manifest['src/oportunidades.js']['file'] ), array(), null, true );
+            wp_enqueue_script_module( $manifest['src/oportunidades.js']['name'], get_parent_theme_file_uri( $manifest['src/oportunidades.js']['file'] ), array(), false, array( 'in_footer' => true ) );
         }
 
         if (is_singular( 'curso' )) {
-            wp_enqueue_script( $manifest['src/curso.js']['name'], get_parent_theme_file_uri( $manifest['src/curso.js']['file'] ), array(), null, true );
+            wp_enqueue_script_module( $manifest['src/curso.js']['name'], get_parent_theme_file_uri( $manifest['src/curso.js']['file'] ), array(), false, array( 'in_footer' => true ) );
         }
 
         /* VLibras */
         if (!WP_DEBUG) {
-            wp_enqueue_script( 'vlibras', 'https://vlibras.gov.br/app/vlibras-plugin.js', array(), null, true );
+            wp_enqueue_script( 'vlibras', 'https://vlibras.gov.br/app/vlibras-plugin.js', array(), false, true );
             wp_add_inline_script( 'vlibras', "new window.VLibras.Widget('https://vlibras.gov.br/app');" );
         }
     }, 11 );
