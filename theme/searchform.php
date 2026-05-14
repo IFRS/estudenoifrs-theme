c6b6fe427763bae326185fa53bd480af476aab57
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-05-14 15:25:40 -0300

    Remoção de estilos desnecessários

--- theme/searchform.php
+++ theme/searchform.php
@@ -1,10 +1,10 @@
-<form role="search" method="get" class="searchform row row-cols-sm-auto g-3 align-items-center" action="<?php echo esc_url(home_url('/')); ?>">
+<form role="search" method="get" class="searchform" action="<?php echo esc_url(home_url('/')); ?>">
     <?php $idBusca = uniqid(); ?>
     <a href="#inicio-busca" id="inicio-busca" class="visually-hidden">In&iacute;cio da busca</a>
     <label class="visually-hidden" for="<?php echo $idBusca; ?>">Buscar por:</label>
     <div class="input-group">
-        <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="<?php echo $idBusca; ?>" class="form-control form-control-sm searchform__input" placeholder="Buscar em todo o site" required>
-        <button type="submit" class="btn btn-light btn-sm searchform__submit">
+        <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="<?php echo $idBusca; ?>" class="form-control searchform__input" placeholder="Buscar em todo o site" required>
+        <button type="submit" class="btn btn-light searchform__submit">
             <i class="fa-solid fa-magnifying-glass"></i>
         </button>
     </div>
