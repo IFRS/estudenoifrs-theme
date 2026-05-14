d58851808a84aef8d1a2c6ffd2836330638463c8
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-05-14 15:20:17 -0300

    Alteração na forma de usar o FontAwesome 7

--- theme/searchform.php
+++ theme/searchform.php
@@ -5,11 +5,7 @@
     <div class="input-group">
         <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="<?php echo $idBusca; ?>" class="form-control form-control-sm searchform__input" placeholder="Buscar em todo o site" required>
         <button type="submit" class="btn btn-light btn-sm searchform__submit">
-            <?php $idSVG = uniqid(); ?>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="#000000" aria-labelledby="<?php echo $idSVG; ?>">
-                <title id="<?php echo $idSVG; ?>">Buscar</title>
-                <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"/>
-            </svg>
+            <i class="fa-solid fa-magnifying-glass"></i>
         </button>
     </div>
 </form>
