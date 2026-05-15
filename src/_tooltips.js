26811a235648ee68e3daa88a09cd7d83c395a866
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-05-15 09:29:30 -0300

    Importação somente de módulo usados do Bootstrap

--- src/_tooltips.js
+++
@@ -1,7 +0,0 @@
-import Tooltip from 'bootstrap/js/dist/tooltip.js';
-
-let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-
-let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new Tooltip(tooltipTriggerEl);
-});
